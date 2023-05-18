356cfeda7339c1455e8700df2237cd8f2ae40d88
acdhirr <acdhirr@trilobiet.nl>
2023-05-18 17:51:50 +0200

    Sample Python scripts

--- sample-scripts/python/shmp_onix.py
+++ sample-scripts/python/shmp_onix.py
@@ -1,7 +1,5 @@
 # This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Run a query, concatenate the results and save as a file.
 
 from datetime import datetime
 today: str = datetime.today().strftime('%Y%m%d')
