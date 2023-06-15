380f6f687c62be0a23339689511be3ea8af4ad04
acdhirr <acdhirr@trilobiet.nl>
2023-06-15 15:45:09 +0200

    Wirefram backoffice + DB

--- documentation/db/oapen_memo_manager/scripts/data_model1_table_create_MYSQL.sql
+++ documentation/db/oapen_memo_manager/scripts/data_model1_table_create_MYSQL.sql
@@ -2,9 +2,9 @@ CREATE TABLE oapen_memo_manager.client (
     id VARCHAR(32) NOT NULL,
     fullname VARCHAR(255) NOT NULL,
     username VARCHAR(45) NOT NULL,
+    oapen_id VARCHAR(255),
     password VARCHAR(255) NOT NULL,
     is_editable boolean NOT NULL,
-    oapen_id VARCHAR(255),
     notes text,
     PRIMARY KEY (id)
 );
@@ -16,7 +16,7 @@ ALTER TABLE oapen_memo_manager.client
 CREATE TABLE oapen_memo_manager.query (
     id VARCHAR(32) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    body text NOT NULL,
+    body text,
     params text,
     id_script VARCHAR(32) NOT NULL,
     PRIMARY KEY (id)
@@ -29,19 +29,23 @@ CREATE INDEX part_of_id_script ON oapen_memo_manager.query
 CREATE TABLE oapen_memo_manager.script (
     id VARCHAR(32) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    type ENUM('MAIN','SUB') NOT NULL,
-    body text NOT NULL,
+    type ENUM('MAIN','SNIP') NOT NULL,
+    body text,
     params text,
     PRIMARY KEY (id)
 );
+
 
 CREATE TABLE oapen_memo_manager.task (
     id VARCHAR(32) NOT NULL,
     name VARCHAR(100) NOT NULL,
     start_date date NOT NULL,
     frequency ENUM('D','W','M','Y') NOT NULL,
-    id_client VARCHAR(32) NOT NULL,
     is_active boolean NOT NULL,
+    last_run_date date,
+    is_last_run_success boolean,
+    last_run_message text,
+    id_client VARCHAR(32) NOT NULL,
     id_script VARCHAR(32) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -52,5 +56,20 @@ ALTER TABLE oapen_memo_manager.task
 CREATE INDEX part_of_id_client ON oapen_memo_manager.task
     (id_client);
 
+
+CREATE TABLE oapen_memo_manager.runlog (
+    id INTEGER NOT NULL,
+    date date NOT NULL,
+    is_success boolean NOT NULL,
+    message text,
+    id_task VARCHAR(32) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX part_of_id_task ON oapen_memo_manager.runlog
+    (id_task);
+
+
 ALTER TABLE oapen_memo_manager.task ADD CONSTRAINT FK_task__id_client FOREIGN KEY (id_client) REFERENCES oapen_memo_manager.client(id);
 ALTER TABLE oapen_memo_manager.task ADD CONSTRAINT FK_task__id_script FOREIGN KEY (id_script) REFERENCES oapen_memo_manager.script(id);
+ALTER TABLE oapen_memo_manager.runlog ADD CONSTRAINT FK_runlog__id_task FOREIGN KEY (id_task) REFERENCES oapen_memo_manager.task(id);
